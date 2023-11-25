bce4019e505f7f4901109bac3eda886e6b4c4e91
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-11-25 08:27:58 +0000

    Update log4j-core, ... to 2.22.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
 
   lazy val awsDynamoDbClient = "uk.gov.nationalarchives" %% "da-dynamodb-client" % "0.1.23"
   lazy val awsEventBridgeClient = "uk.gov.nationalarchives" %% "da-eventbridge-client" % "0.1.23"
-  lazy val logbackVersion = "2.20.0"
+  lazy val logbackVersion = "2.22.0"
   lazy val dynamoFormatters = "uk.gov.nationalarchives" %% "dynamo-formatters" % "0.0.1"
   lazy val log4jSlf4j = "org.apache.logging.log4j" % "log4j-slf4j-impl" % logbackVersion
   lazy val log4jCore = "org.apache.logging.log4j" % "log4j-core" % logbackVersion
